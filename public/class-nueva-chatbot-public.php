fa679a2cefef084c92ef3c782501f62400bd843c
Nueva Digital Solution <nuevadigital.india@gmail.com>
2025-12-28 16:46:57 +0530

    Changes Done

--- public/class-nueva-chatbot-public.php
+++ public/class-nueva-chatbot-public.php
@@ -121,11 +121,20 @@ class Nueva_Chatbot_Public
                     </div>
                 </div>
                 <div class="nueva-chat-footer">
-                    <input type="text" id="nueva-chat-input" placeholder="Type a message...">
-                    <button id="nueva-chat-send">Send</button>
-                    <button id="nueva-chat-end" title="End Chat & Email Transcript" class="nueva-end-chat-btn">
-                        End Chat &times;
-                    </button>
+                    <div class="nueva-footer-controls">
+                        <input type="text" id="nueva-chat-input" placeholder="Type a message...">
+                        <button id="nueva-chat-send" title="Send Message">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="16"
+                                height="16">
+                                <path
+                                    d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z">
+                                </path>
+                            </svg>
+                        </button>
+                        <button id="nueva-chat-end" title="End Chat & Email Transcript" class="nueva-end-chat-btn">
+                            End
+                        </button>
+                    </div>
                     <!-- Branding: Critical Requirement -->
                     <div class="nueva-powered-by">
                         <a href="https://nuevadigital.co.in" target="_blank" id="nueva-branding-link">Powered by Nueva
